5944b0e821497926b6a15873422dfb6b0ca53c92
HimashiKavindee <115772118+HimashiKavindee@users.noreply.github.com>
2024-10-25 22:11:24 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class BubbleSort{
+       public static void main(String[]args){
+	   int i=3;
+	   int j=4;
+	   System.out.println("before swapping");
+	   System.out.println( "i: " +i);
+	   System.out.println( "j: " +j);
+	   
+	   int temp=i;
+	   i=j;
+	   j=temp;
+	   
+	   System.out.println("after swapping");
+	   System.out.println( "i: " +i);
+	   System.out.println( "j: " +j);
+	   }
+}	   
+
