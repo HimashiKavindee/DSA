b2ac02aa83a148eef5ecff5f4e9ffb8759409a89
HimashiKavindee <115772118+HimashiKavindee@users.noreply.github.com>
2024-12-07 11:13:00 +0530

    Add files via upload

---
+++ FindFactorial.java
@@ -0,0 +1,14 @@
+public  class FindFactorial{
+        public static long factorial (int n){
+		       if(n==1)
+			      return 1;
+			   else
+                  return n*factorial(n-1);
+}				  
+ public static void main(String[]args){
+		       long result= factorial(100);
+			   System.out.println("Factorial is " + result);
+
+     }
+}
+			   
