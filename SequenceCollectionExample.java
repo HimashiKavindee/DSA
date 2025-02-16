7da3161cb40b0bf1996f3d6f93f4db3308819a9e
HimashiKavindee <115772118+HimashiKavindee@users.noreply.github.com>
2025-02-16 12:10:30 +0530

    Add files via upload

---
+++ SequenceCollectionExample.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.SequencedCollection;
+
+public class SequenceCollectionExample{
+	public SequenceCollectionExample(){
+		SequencedCollection sc = new  LinkedList();
+		sc.addFirst(5);
+		sc.addFirst(10);
+		sc.addLast(3);
+		System.out.println(sc.getFirst());
+		System.out.println(sc.removeLast());
+		System.out.println(sc);
+		System.out.println(sc.reversed());	
+	}
+	public static void main(String []args)
+	{
+		new SequanceCollectionExample();
+	}
+}
